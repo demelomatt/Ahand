b6f1b9945c799f4e80f0f652ea4c609c6a98e48d
Matheus Melo <demelosmatheus@outlook.com>
2020-07-05 12:10:40 -0300

    Update funções interface gráfica

--- main.py
+++ main.py
@@ -1,207 +1,218 @@
-# Bibliotecas externas
-import pytesseract,fitz
-from PIL import Image
-# Bibliotecas nativas do Python
-import os, shutil, re, datetime, zipfile, csv, codecs
+################################################################################
+##
+## BY: WANDERSON M.PIMENTA
+## PROJECT MADE WITH: Qt Designer and PySide2
+## V: 1.0.0
+##
+################################################################################
 
-# Atribuição de variáveis globais
-username = os.path.expanduser("~") 
-dateTime = (datetime.datetime.now()) 
-splitDateTime = [str(dateTime.year),str(dateTime.month)] 
-fullDirNames, dirNamesWithoutExt, pdfFilenames, groups, keywordsByGroup, keywords, currentKeyword, searchForKeyword = [],[],[],[],[],[],"",""
+import sys
+import platform
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
 
-# Atribuição de funções
-def listFiles(path,ext): # Listar arquivos PDF de um diretório
-    global fullDirNames, dirNamesWithoutExt, pdfFilenames
-    fullDirNames = [] 
-    dirNamesWithoutExt = []
-    pdfFilenames = []
-    files = os.listdir(path)
-    for file in files:
-        if file.endswith(ext):
-            fullDirNames.append(path + file)
-            dirNamesWithoutExt.append(file)
-            pdfFilenames.append(os.path.splitext(file)[0])
+# GUI FILE
+from ui_main import Ui_MainWindow
 
-def ocrPdf(path): # Transformar arquivos PDF em pesquisáveis
-    createDirectory(inputDirectory)
-    createDirectory(inputDirectory + "00 -- OCR\\") 
-    listFiles(path,".pdf")
-    filesNumber = len(fullDirNames)
-    print("Arquivos encontrados para converter:",filesNumber)
-    print("Carregando...")
-    if filesNumber != 0:
-        counterFiles, TotalPagesNumber, loadPdfPage, openPDF, zoom = 0, 0,"","", 4
-        matrixZoom = fitz.Matrix(zoom,zoom)
-        for file in fullDirNames: 
-            openPDF = fitz.open(file)
-            TotalPagesNumber = openPDF.pageCount
-            counterPages = 0
-            while counterPages < TotalPagesNumber: 
-                loadPdfPage = openPDF.loadPage(counterPages)
-                pix = loadPdfPage.getPixmap(matrixZoom)
-                filename = inputDirectory + "00 -- OCR\\" + os.path.splitext(dirNamesWithoutExt[counterFiles])[0] + str(counterPages +1) 
-                pix.writeImage(filename + ".jpeg","jpeg") 
-                pdfOcr = pytesseract.image_to_pdf_or_hocr(filename + ".jpeg", lang = "por", extension = "pdf") 
-                with open(filename + ".pdf","w+b") as f:
-                    f.write(pdfOcr)
-                os.remove(filename + ".jpeg")
-                print("Página {} do arquivo {} convertida com sucesso.".format(counterPages +1,dirNamesWithoutExt[counterFiles]))
-                counterPages += 1
-            counterFiles += 1
-        print("Sucesso ao converter arquivos.")
+# IMPORT QSS CUSTOM
+from ui_styles import Style
 
-def searchAndExport(): # Pesquisar por palavras dentro de PDF e mover arquivo caso encontrar
-    global searchForKeyword,currentKeyword
-    def export(): # Mover arquivo
-        outputName = groups[counterGroups] + "\\" + splitDateTime[0] + "-" + splitDateTime[1] + " " + pdfFilenames[counterFiles] + " - " + keywords[counterKeywords] + ".pdf" 
-        createDirectory(outputDirectory)
-        createDirectory(outputDirectory + groups[counterGroups])
-        try:
-            moveFiles(file, outputDirectory + outputName) 
-        except: 
-            openPDF.close() 
-            moveFiles(file, outputDirectory + outputName) 
-        print("A palavra {} foi encontrada no arquivo {} e movida para a pasta {}.".format(keywords[counterKeywords],file,groups[counterGroups]))
-    
-    def inputData(): # Importar dados através de arquivo CSV
-        global groups, keywordsByGroup, keywords
-        csvFile = codecs.open("C:\\Program Files (x86)\\Ahand\\keywords.csv", encoding = "utf-8") 
-        reader = csv.reader(csvFile) 
-        list_of_rows = list(reader)
-        groups = list_of_rows[0]
-        keywordsByGroup = list_of_rows[1] 
-        keywords = (list_of_rows[2]) 
+# IMPORT FUNCTIONS
+from ui_functions import *
 
-    def regex(): # Pesquisar por palavras com e sem acento
-        global currentKeyword,searchForKeyword
-        symbols = ["Ã","Â","Á","À","Ê","É","È","Í","Õ","Ô","Ó","Ò","Ú","Ù","Ç"] 
-        symbolsByLetters = [4,3,1,4,2,1] 
-        letters = ["A","E","I","O","U","C"] 
-        counterLetters = 0 
-        counterSymbols = 0 
-        newKeyword = currentKeyword 
-        while counterLetters < len(letters): 
-            counterSymbolsByLetters = 0
-            while counterSymbolsByLetters < symbolsByLetters[counterLetters]: 
-                newKeyword = re.sub(symbols[counterSymbols],letters[counterLetters],newKeyword)
-                counterSymbolsByLetters += 1
-                counterSymbols +=1
-            counterLetters += 1
-        if (newKeyword != currentKeyword): 
-            print("Palavra pesquisada: ",newKeyword)            
-            searchForKeyword = page.searchFor(newKeyword, hit_max = 16) 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-    def remainingFiles(path): # Arquivos não encontrados
-        files = os.listdir(inputDirectory + "00 -- OCR\\")
-        if files != []:
-            createDirectory(outputDirectory + "00 -- NOT FOUNDED\\")
-            for file in files:
-                try:
-                    moveFiles(inputDirectory + "00 -- OCR\\"+ file,outputDirectory + "00 -- NOT FOUNDED\\" + file) 
-                except Exception as e:
-                    print(e)
-                    pass
-    inputData()
-    listFiles(inputDirectory + "00 -- OCR\\",".pdf")
-    files = fullDirNames
-    print("Arquivos encontrados para pesquisar:",len(files))
-    counterFiles = 0
-    if (files == []): 
-        print("Não existem arquivos PDF na pasta {}\nInsira os arquivos e tecle algo para continuar...".format(inputDirectory))
-        input()
-        ocrPdf(inputDirectory)
-        listFiles(inputDirectory + "00 -- OCR\\",".pdf")
-        files = fullDirNames  
-    else:
-        print("Pesquisando por palavras...")
-    for file in files: 
-        print("Pesquisando em arquivo: {}".format(file))
-        openPDF = fitz.open(file) 
-        page = openPDF.loadPage(0) 
-        counterKeywords = 0 
-        counterGroups = 0 
-        foundBoolean = 0 
-        while counterGroups < len(groups) and (foundBoolean==0): 
-            print("Total de palavras para pesquisar: {} de {}.".format(counterKeywords+1,len(keywords)))
-            print("Pesquisando grupo {} de {}.".format(counterGroups+1,len(groups)))
-            while counterKeywords < len(keywords) and (foundBoolean ==0): 
-                print("Grupo: ",groups[counterGroups])
-                print("Palavras-chave nesse grupo: {}".format(keywordsByGroup[counterGroups]))
-                counterKeywordsByGroup = 0 
-                while counterKeywordsByGroup < int(keywordsByGroup[counterGroups]): 
-                    print("Pesquisando palavra {} de {}.".format(counterKeywordsByGroup+1,int(keywordsByGroup[counterGroups])))
-                    currentKeyword = keywords[counterKeywords] 
-                    print("Palavra pesquisada: ",currentKeyword)
-                    searchForKeyword = page.searchFor(currentKeyword, hit_max = 16) 
-                    if (searchForKeyword!= []): 
-                        export()
-                        foundBoolean = 1
-                        break
-                    else:
-                        regex()
-                        if (searchForKeyword!= []):
-                            export()
-                            foundBoolean = 1
-                            break
-                        else:
-                            print("A palavra {} não foi encontrada neste arquivo.".format(keywords[counterKeywords]))
-                    counterKeywords += 1
-                    counterKeywordsByGroup +=1
-                counterGroups += 1
-        counterFiles += 1
-    remainingFiles(inputDirectory + "00 -- OCR\\")
-    if (files != []): 
-        print("Sucesso ao encontrar palavras e exportar arquivos.")
+        ########################################################################
+        ## START - WINDOW ATTRIBUTES
+        ########################################################################
 
-def zipCompress(path): # Compactar arquivos em ZIP
-    for root, subdirs, files in os.walk(path):
-        for subdir in subdirs:
-            if ((subdir != "00 -- NOT FOUNDED") and (subdir != "00 -- ZIP FILES")):
-                outputPath = outputDirectory + "00 -- ZIP FILES\\"
-                createDirectory(outputPath) 
-                zipFilename =  subdir + ".zip" 
-                createZipFile = zipfile.ZipFile(zipFilename, "w")
-                listFiles(path + subdir + "\\",".pdf")
-                for file in fullDirNames:
-                    createZipFile.write(file,os.path.basename(file))
-                createZipFile.close()
-                moveFiles(zipFilename,outputPath + zipFilename) 
-                print("Arquivo {} movido de {} para {}".format(zipFilename,os.path.join(path, subdir),outputPath))
+        ## REMOVE ==> STANDARD TITLE BAR
+        UIFunctions.removeTitleBar(True)
+        ## ==> END ##
 
-def moveFiles(fromPath,toPath): # Mover arquivos
-    if not os.path.exists(toPath):    
-        shutil.move(fromPath,toPath)
-    if os.path.exists(fromPath):
-        os.remove(fromPath)
+        ## SET ==> WINDOW TITLE
+        self.setWindowTitle('Ahand')
+        UIFunctions.labelTitle(self, 'Ahand')
+        UIFunctions.labelDescription(self, 'Giving you a hand for automation.')
+        ## ==> END ##
 
-def createDirectory(path): # Criar diretórios se não existirem
-    if not os.path.exists(path):
-        os.mkdir(path)
+        ## REMOVE ==> STANDARD TITLE BAR
+        startSize = QSize(1000, 720)
+        self.resize(startSize)
+        self.setMinimumSize(startSize)
+        # UIFunctions.enableMaximumSize(self, 500, 720)
+        ## ==> END ##
 
-def runMain(): # Função que engloba as principais funções
-    ocrPdf(inputDirectory)
-    searchAndExport()
-    zipCompress(outputDirectory)
-    print("Processos concluídos com sucesso.")
+        ## ==> CREATE MENUS
+        ########################################################################
 
-# Verificar se diretórios existem
-try:
-    rootDirectory = username + "\\Onedrive\\Desktop\\\Ahand\\"
-    createDirectory(rootDirectory)
-except:
-    try:
-        rootDirectory = username + "\\Onedrive\\Área de Trabalho\\Ahand\\"
-        createDirectory(rootDirectory)
-    except:
-        try:
-            rootDirectory = username + "\\Desktop\\Ahand\\"
-            createDirectory(rootDirectory)
-        except:
-            rootDirectory = username + "\\Área de Trabalho\\Ahand\\"
-            createDirectory(rootDirectory)
-            
-inputDirectory = rootDirectory + "input\\"
-outputDirectory = rootDirectory + "output\\"
+        ## ==> TOGGLE MENU SIZE
+        self.ui.pushButton_toggle_menu.clicked.connect(lambda: UIFunctions.toggleMenu(self, 220, True))
+        ## ==> END ##
 
-runMain()
+        ## ==> ADD CUSTOM MENUS
+        self.ui.stackedWidget.setMinimumWidth(20)
+        UIFunctions.addNewMenu(self, "Merge", "pushButton_merge", "url(:/16x16/icons/16x16/cil-home.png)", True)
+        UIFunctions.addNewMenu(self, "Scan", "pushButton_scan", "url(:/16x16/icons/16x16/cil-user-follow.png)", True)
+        UIFunctions.addNewMenu(self, "Search Patterns", "pushButton_searchpatterns", "url(:/16x16/icons/16x16/cil-user-follow.png)", True)
+        UIFunctions.addNewMenu(self, "Custom Widgets", "pushButton_widgets", "url(:/16x16/icons/16x16/cil-equalizer.png)", False)
+        ## ==> END ##
+
+        # START MENU => SELECTION
+        UIFunctions.selectStandardMenu(self, "pushButton_merge")
+        ## ==> END ##
+
+        ## ==> START PAGE
+        self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+        ## ==> END ##
+
+        ## USER ICON ==> SHOW HIDE
+        UIFunctions.userIcon(self, "WM", "url(:/16x16/icons/16x16/cil-user.png)", False)
+        ## ==> END ##
+
+        ## ==> MOVE WINDOW / MAXIMIZE / RESTORE
+        ########################################################################
+        def moveWindow(event):
+            # IF MAXIMIZED CHANGE TO NORMAL
+            if UIFunctions.returStatus() == 1:
+                UIFunctions.maximize_restore(self)
+
+            # MOVE WINDOW
+            if event.buttons() == Qt.LeftButton:
+                self.move(self.pos() + event.globalPos() - self.dragPos)
+                self.dragPos = event.globalPos()
+                event.accept()
+
+        # WIDGET TO MOVE
+        self.ui.frame_label_top_btns.mouseMoveEvent = moveWindow
+        ## ==> END ##
+
+        ## ==> LOAD DEFINITIONS
+        ########################################################################
+        UIFunctions.uiDefinitions(self)
+        ## ==> END ##
+
+        ########################################################################
+        ## END - WINDOW ATTRIBUTES
+        ############################## ---/--/--- ##############################
+
+
+
+
+        ########################################################################
+        #                                                                      #
+        ## START -------------- WIDGETS FUNCTIONS/PARAMETERS ---------------- ##
+        #                                                                      #
+        ## ==> USER CODES BELLOW                                              ##
+        ########################################################################
+
+
+
+        ## ==> QTableWidget PARAMETERS
+        ########################################################################
+
+        self.ui.tableWidget.horizontalHeader().setSectionResizeMode(0,QtWidgets.QHeaderView.ResizeToContents)
+        self.ui.pushButton_add.clicked.connect(lambda: UIFunctions.insertRow(self))
+        self.ui.pushButton_delete.clicked.connect(lambda: UIFunctions.deleteRow(self))
+        self.ui.pushButton_save.clicked.connect(lambda: UIFunctions.saveData(self))
+
+        ## ==> END ##
+
+        ########################################################################
+        #                                                                      #
+        ## END --------------- WIDGETS FUNCTIONS/PARAMETERS ----------------- ##
+        #                                                                      #
+        ############################## ---/--/--- ##############################
+
+
+        ## SHOW ==> MAIN WINDOW
+        ########################################################################
+        self.show()
+        ## ==> END ##
+
+    ########################################################################
+    ## MENUS ==> DYNAMIC MENUS FUNCTIONS
+    ########################################################################
+    def Button(self):
+        # GET BT CLICKED
+        pushButton_Widget = self.sender()
+
+        # PAGE MERGE
+        if pushButton_Widget.objectName() == "pushButton_merge":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+            UIFunctions.resetStyle(self, "pushButton_merge")
+            UIFunctions.labelPage(self, "Merge")
+            pushButton_Widget.setStyleSheet(UIFunctions.selectMenu(pushButton_Widget.styleSheet()))
+
+        # PAGE SCAN
+        if pushButton_Widget.objectName() == "pushButton_scan":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_scanner)
+            UIFunctions.resetStyle(self, "pushButton_scan")
+            UIFunctions.labelPage(self, "Escanear")
+            pushButton_Widget.setStyleSheet(UIFunctions.selectMenu(pushButton_Widget.styleSheet()))
+
+        # PAGE  SEARCH PATTERNS
+        if pushButton_Widget.objectName() == "pushButton_searchpatterns":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_searchpatterns)
+            UIFunctions.resetStyle(self, "pushButton_searchpatterns")
+            UIFunctions.labelPage(self, "Procurar padrões")
+            pushButton_Widget.setStyleSheet(UIFunctions.selectMenu(pushButton_Widget.styleSheet()))
+
+
+    ## ==> END ##
+
+    ########################################################################
+    ## START ==> APP EVENTS
+    ########################################################################
+
+    ## EVENT ==> MOUSE DOUBLE CLICK
+    ########################################################################
+    def eventFilter(self, watched, event):
+        if watched == self.le and event.type() == QtCore.QEvent.MouseButtonDblClick:
+            print("pos: ", event.pos())
+    ## ==> END ##
+
+    ## EVENT ==> MOUSE CLICK
+    ########################################################################
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPos()
+        if event.buttons() == Qt.LeftButton:
+            print('Mouse click: LEFT CLICK')
+        if event.buttons() == Qt.RightButton:
+            print('Mouse click: RIGHT CLICK')
+        if event.buttons() == Qt.MidButton:
+            print('Mouse click: MIDDLE BUTTON')
+    ## ==> END ##
+
+    ## EVENT ==> KEY PRESSED
+    ########################################################################
+    def keyPressEvent(self, event):
+        print('Key: ' + str(event.key()) + ' | Text Press: ' + str(event.text()))
+    ## ==> END ##
+
+    ## EVENT ==> RESIZE EVENT
+    ########################################################################
+    def resizeEvent(self, event):
+        self.resizeFunction()
+        return super(MainWindow, self).resizeEvent(event)
+
+    def resizeFunction(self):
+        print('Height: ' + str(self.height()) + ' | Width: ' + str(self.width()))
+    ## ==> END ##
+
+    ########################################################################
+    ## END ==> APP EVENTS
+    ############################## ---/--/--- ##############################
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    QtGui.QFontDatabase.addApplicationFont('fonts/segoeui.ttf')
+    QtGui.QFontDatabase.addApplicationFont('fonts/segoeuib.ttf')
+    window = MainWindow()
+    sys.exit(app.exec_())
